c8413dc4161e22affd806d5dd7f16be995a092fd
AncaEmilia <AncaEmilia@users.noreply.github.com>
2021-10-04 12:43:39 +0300

    prepare config file to heroku

---
+++ src/configs/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAR1qFkoOCE0tm6FjKP-qjN_KUf3p4RblU",
+    authDomain: "eshop-d325c.firebaseapp.com",
+    projectId: "eshop-d325c",
+    storageBucket: "eshop-d325c.appspot.com",
+    messagingSenderId: "711418379952",
+    appId: "1:711418379952:web:21bd030bbe71ea6b350575"
+  };
+
+  export default firebaseConfig;
